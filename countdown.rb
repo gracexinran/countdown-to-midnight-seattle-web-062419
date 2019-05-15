9bf8434e6753815cc0380fdb56ffeaeceec2d9cc
Xinran Che <gracexinran@gmail.com>
2019-05-15 05:35:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  count = integer
+  while count > 0
+    puts "#{count} SECOND(S)!"
+    count -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
